adefff6808f38b84d81585f382fbc285bfff2545
paveldelei3020 <paveldelei200@gmail.com>
2025-03-04 15:53:58 +0300

    step5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,25 @@
-const rondomaizer=()=>{
+import readlineSync from 'readline-sync';
+import { user } from "../src/cli.js"
+const game=()=>{
     let min=1;
     let max=100;
-    return Math.floor(Math.random()*(max-min));
+    console.log('Welcome to the Brain Games!');
+    const userName = user();
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    let answercount=0;
+    while(answercount < 3){
+        const rondomaizer=Math.floor(Math.random()*(max-min));
+        console.log ('Question: '+rondomaizer);
+        
+        const answer = readlineSync.question('Your answer: ');
+        const coranswer = (rondomaizer % 2 === 0) ?  'yes' : 'no' ;
+        if (answer!==coranswer){
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${coranswer}'.\nLet's try again, ${userName}!`)
+            return;
+        }
+        console.log('Correct!')
+        answercount +=1
+    }
+    return `Congratulations, ${userName}!`
 }
-
-const game=()=>{
-    if (rondomaizer%0)
-}
+console.log(game())
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 export const user=()=>{
     const userName = readlineSync.question('May I have your name? ');
-    return 'Hello, ' + userName + '!';
+    console.log('Hello, ' + userName + '!');
+    return userName;
 }
